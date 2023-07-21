93f947a7be56fd42cd3c6b18c3e4ee9a0b00f912
DocCreeps <dorian1998dino@hotmail.com>
2023-07-21 10:38:42 +0200

    Design counter

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,7 +1,9 @@
-<div>
-    <h1>{{ $count }}</h1>
+<div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-4xl">{{ $count }}</h1>
 
-    <button wire:click="increment">+</button>
+    <div class="mt-4">
+        <button wire:click="increment" class="px-4 py-2 bg-blue-500 text-white rounded">+</button>
 
-    <button wire:click="decrement">-</button>
+        <button wire:click="decrement" class="px-4 py-2 bg-red-500 text-white rounded">-</button>
+    </div>
 </div>
